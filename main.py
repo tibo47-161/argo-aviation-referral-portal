cca0d7062bd9b462e1e38b943eee10f0d5fc385c
tibo47-161 <179575773+tibo47-161@users.noreply.github.com>
2025-10-02 08:40:45 -0400

    Version 12: Save current changes

--- main.py
+++ main.py
@@ -628,4 +628,4 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 3000)), debug=True)
